e9c90433f34d1bec66b38d016d038ee1f32b4850
dosinhuda <dosinhuda@gmail.com>
2021-03-26 08:34:18 +0700

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -1,7 +1,7 @@
 pragma  solidity 0.6.1;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract MyToken is ERC20, ERC20Detailed {
     constructor(uint256 initialSupply) ERC20Detailed("StarDucks Capu-Token", "SCT", 0) public {
